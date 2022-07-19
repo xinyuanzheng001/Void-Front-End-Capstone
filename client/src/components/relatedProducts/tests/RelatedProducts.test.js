3dba935696532a20a1094bd1bd7900b7993b2e58
Brandon Hester <brandonrhester@gmail.com>
2022-07-19 14:46:27 -0400

    initial relatedproducts test

---
+++ client/src/components/relatedProducts/tests/RelatedProducts.test.js
@@ -0,0 +1,11 @@
+console.log('STOPS??????????????????????????????')
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import RelatedProducts from '../RelatedProducts';
+
+describe('RelatedProducts', () => {
+  test('renders RelatedProducts component', () => {
+    render(<RelatedProducts />);
+  });
+});
